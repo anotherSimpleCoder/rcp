f0a88d7c43754a65bb3c89a73de40ee4533c74e3
Amsakan Bavan <amsakan.bavan@softwareag.com>
2024-10-08 15:12:33 +0200

    Added dialog

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,4 +1,5 @@
 mod file_handler;
+mod gui;
 
 use std::io::Write;
 use std::net::TcpStream;
@@ -7,12 +8,33 @@ use miniz_oxide::deflate::compress_to_vec;
 pub fn send(filename: &str, destination_addr: &str) {
     let file_data = file_handler::read_file(filename);
     let compressed_file_data = compress_to_vec(file_data.as_slice(), 10);
+    let (address, new_filename)  = parse_address(destination_addr);
+    let header: Vec<u8> = header(new_filename);
+    let data: Vec<u8> = data(header, compressed_file_data);
 
-    let mut header: Vec<u8> = Vec::new();
-    header.extend(filename.len().to_be_bytes());
-    header.extend("FNME".as_bytes());
-    header.extend(filename.as_bytes());
+    let mut stream = TcpStream::connect(format!("{}:3000", address).as_str())
+        .expect(format!("ClientError: Couldn't connect to {}\n", address).as_str());
 
+    stream.write_all(data.as_slice())
+        .expect("ClientError: Couldn't send files to server.");
+
+    gui::message("rcp", "File request sent!");
+}
+
+fn parse_address(address: &str) -> (&str, &str) {
+    let spliited: Vec<&str> = address.split(':').collect();
+
+    (spliited[0], spliited[1])
+}
+fn header(filename: &str) -> Vec<u8> {
+    let mut headers = Vec::new();
+    headers.extend(filename.len().to_be_bytes());
+    headers.extend("FNME".as_bytes());
+    headers.extend(filename.as_bytes());
+
+    headers
+}
+fn data(header: Vec<u8>, compressed_file_data: Vec<u8>) -> Vec<u8> {
     let mut data: Vec<u8> = Vec::new();
     data.extend((header.len() as u32).to_be_bytes());
     data.extend("RHDR".as_bytes());
@@ -21,11 +43,5 @@ pub fn send(filename: &str, destination_addr: &str) {
     data.extend("RDAT".as_bytes());
     data.extend(compressed_file_data);
 
-
-    let mut stream = TcpStream::connect(format!("{}:3000", destination_addr).as_str())
-        .expect(format!("ClientError: Couldn't connect to {}\n", destination_addr).as_str());
-
-    stream.write_all(data.as_slice())
-        .expect("ClientError: Couldn't send files to server.");
-
+    data
 }
