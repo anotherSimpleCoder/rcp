16e366d881822388b443704f4422285c758cf98c
Amsakan Bavan <amsakan.bavan@softwareag.com>
2024-10-08 15:54:41 +0200

    Changed syntax

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -8,11 +8,11 @@ use miniz_oxide::deflate::compress_to_vec;
 pub fn send(filename: &str, destination_addr: &str) {
     let file_data = file_handler::read_file(filename);
     let compressed_file_data = compress_to_vec(file_data.as_slice(), 10);
-    let (address, new_filename)  = parse_address(destination_addr);
+    let (new_filename, address)  = parse_address(destination_addr);
     let header: Vec<u8> = header(new_filename);
     let data: Vec<u8> = data(header, compressed_file_data);
 
-    let mut stream = TcpStream::connect(format!("{}:3000", address).as_str())
+    let mut stream = TcpStream::connect(address)
         .expect(format!("ClientError: Couldn't connect to {}\n", address).as_str());
 
     stream.write_all(data.as_slice())
@@ -22,9 +22,14 @@ pub fn send(filename: &str, destination_addr: &str) {
 }
 
 fn parse_address(address: &str) -> (&str, &str) {
-    let spliited: Vec<&str> = address.split(':').collect();
+    let splitted: Vec<&str> = address.split('@').collect();
+    let (filename, mut address) = (splitted[0], splitted[1]);
 
-    (spliited[0], spliited[1])
+    if !address.contains(':') {
+        address = format!("{}:3000", address).as_str();
+    }
+
+    (filename, address)
 }
 fn header(filename: &str) -> Vec<u8> {
     let mut headers = Vec::new();
